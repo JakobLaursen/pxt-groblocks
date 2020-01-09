f12fd25e8c679d0acf5d86ae69df4d99f98a7251
Nikomof <54985320+Nikomof@users.noreply.github.com>
2020-01-09 17:57:06 +0100

    incontrol

--- main.ts
+++ main.ts
@@ -98,33 +98,9 @@ return;
 }
 */
 /////////////////////////////////InBackGround///////////////////////
-let bufboi = [0,0,0,0,0,0]; //buffboi int atm
-function sensData(){
-  let readIn = serial.readString();
-  let inSplit = readIn.split('|');
-  basic.pause(5000);
-  if (inSplit[0] == "a") {
-    bufboi[0] = parseInt(inSplit[1]);
-    bufboi[1] = parseInt(inSplit[2]);
-    basic.showString("A"); //Debugging ReadData with string on successful read
-    basic.pause(200);
 
-  } else if (inSplit[0] == "b") {
-    bufboi[2] = parseInt(inSplit[1]);
-    bufboi[3] = parseInt(inSplit[2]);
-    basic.showString("B"); //Debugging ReadData with string on successful read
-    basic.pause(200);
 
-  } else if (inSplit[0] == "c") {
-    bufboi[4] = parseInt(inSplit[1]);
-    bufboi[5] = parseInt(inSplit[2]);
-    basic.showString("C"); //Debugging ReadData with string on successful read
-    basic.pause(200);
-}
 
-control.inBackground(function () {
-    sensData();
-})
 /////////////////////////////////Buffer////////////////////////////
 
 
@@ -336,3 +312,33 @@ j,time
 init();
 
 }
+
+let bufboi = [0,0,0,0,0,0]; //buffboi int atm
+
+control.inBackground(function () {
+
+
+  function sensData(){
+    let readIn = serial.readString();
+    let inSplit = readIn.split('|');
+    basic.pause(5000);
+    if (inSplit[0] == "a") {
+      bufboi[0] = parseInt(inSplit[1]);
+      bufboi[1] = parseInt(inSplit[2]);
+      basic.showString("A"); //Debugging ReadData with string on successful read
+      basic.pause(200);
+
+    } else if (inSplit[0] == "b") {
+      bufboi[2] = parseInt(inSplit[1]);
+      bufboi[3] = parseInt(inSplit[2]);
+      basic.showString("B"); //Debugging ReadData with string on successful read
+      basic.pause(200);
+
+    } else if (inSplit[0] == "c") {
+      bufboi[4] = parseInt(inSplit[1]);
+      bufboi[5] = parseInt(inSplit[2]);
+      basic.showString("C"); //Debugging ReadData with string on successful read
+      basic.pause(200);
+    }
+  }
+})
