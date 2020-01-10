80b26ca86e63f906f4d12c54fb1a3f1cd09433b4
Nikomof <54985320+Nikomof@users.noreply.github.com>
2020-01-10 11:46:46 +0100

    Updates.

--- main.ts
+++ main.ts
@@ -2,24 +2,24 @@
 
 
 enum airList {
-  //% block="Varmelegeme"
-  heater,
-  //% block="Blæser"
-  fan
-  //%block="Ventilation"
+    //% block="Varmelegeme"
+    heater,
+    //% block="Blæser"
+    fan
+    //%block="Ventilation"
 }
 
 
 enum pumpList {
-  //% block="Water circulation"
-  waterPump,
-  //% block="Airation"
-  airationPump,
-  //% block="External pump 1"
-  fert1Pump,
-  //% block="External pump 2"
-  fert2Pump,
-  //% block="External pump 1"
+    //% block="Water circulation"
+    waterPump,
+    //% block="Airation"
+    airationPump,
+    //% block="External pump 1"
+    fert1Pump,
+    //% block="External pump 2"
+    fert2Pump,
+    //% block="External pump 1"
 }
 
 enum lightList {
@@ -37,250 +37,250 @@ enum lightList {
 //% groups="['Aktuatore', Sensore']"
 namespace groblocks {
 
-// ############################# Init ########################################
-//=============================================================================
-//############################################################################
+    // ############################# Init ########################################
+    //=============================================================================
+    //############################################################################
 
-function init(){
-    serial.redirect(
-    SerialPin.P0,
-    SerialPin.P1,
-    BaudRate.BaudRate9600
-  );
-  pins.digitalWritePin(DigitalPin.P2, 1);
-  basic.showString("K");
-  }
-
-
-// actuCat index
-let lightCat: number = 0;
-let airCat: number = 1;
-let pumpCat: number = 2;
+    function init() {
+        serial.redirect(
+            SerialPin.P0,
+            SerialPin.P1,
+            BaudRate.BaudRate9600
+        );
+        pins.digitalWritePin(DigitalPin.P2, 1);
+        basic.showString("K");
+    }
 
 
-function readData(index: number, listABC: string): number {
-  let readIn = serial.readString();
-  let readOut = readIn.split("|");
-  let readDisp = readOut[index];
-  if (readOut[0] == listABC){
-    basic.showString("X") //Debugging ReadData with string on successful read
-    return parseInt(readDisp);
-  } else {
-    return 0;
-  }
-}
-///////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////BUFFER/////////////////////
-
-/*
-function updateBuffer (){
-let updated = [0,1,1];
-while(updated !=[1,1,1]){
-
-let importString = serial.readString();
-let dataString = importString.split('|');
-
-if (dataString[0] == "a") {
-  bufboi[0] = parseInt(dataString[1]);
-  bufboi[1] = parseInt(dataString[2]);
-  updated[0]=1
-} else if (dataString[0] == "b") {
-  bufboi[2] = parseInt(dataString[1]);
-  bufboi[3] = parseInt(dataString[2]);
-  updated[1]=1
-} else if (dataString[0] == "c") {
-  bufboi[4] = parseInt(dataString[1]);
-  bufboi[5] = parseInt(dataString[2]);
-  updated[2]=1
-}
-}
-return;
-}
-*/
-/////////////////////////////////InBackGround///////////////////////
+    // actuCat index
+    let lightCat: number = 0;
+    let airCat: number = 1;
+    let pumpCat: number = 2;
 
 
+    function readData(index: number, listABC: string): number {
+        let readIn = serial.readString();
+        let readOut = readIn.split("|");
+        let readDisp = readOut[index];
+        if (readOut[0] == listABC) {
+            basic.showString("X") //Debugging ReadData with string on successful read
+            return parseInt(readDisp);
+        } else {
+            return 0;
+        }
+    }
+    ///////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////BUFFER/////////////////////
 
-/////////////////////////////////Buffer////////////////////////////
-
-
-function sendData(actuCat: number, actuType: number, actuSet: number){
-  let output = actuCat.toString() + ";" + actuType.toString() + "," + actuSet.toString();
-  serial.writeString(output);
-  basic.pause(100);
-}
-
-
-// #####################   SENSORS   #################################
-//=============================================================================
-//############################################################################
-
-//ReceivedData:1|sequence|Humidity|Water_Level|CO2|Temp|Door| clock
-//Indexing for readData
+    /*
+    function updateBuffer (){
+    let updated = [0,1,1];
+    while(updated !=[1,1,1]){
+    
+    let importString = serial.readString();
+    let dataString = importString.split('|');
+    
+    if (dataString[0] == "a") {
+      bufboi[0] = parseInt(dataString[1]);
+      bufboi[1] = parseInt(dataString[2]);
+      updated[0]=1
+    } else if (dataString[0] == "b") {
+      bufboi[2] = parseInt(dataString[1]);
+      bufboi[3] = parseInt(dataString[2]);
+      updated[1]=1
+    } else if (dataString[0] == "c") {
+      bufboi[4] = parseInt(dataString[1]);
+      bufboi[5] = parseInt(dataString[2]);
+      updated[2]=1
+    }
+    }
+    return;
+    }
+    */
+    /////////////////////////////////InBackGround///////////////////////
 
 
 
-//A =0
-let hum = 1;
-let water = 2;
-
-//B 0
-//let co2 = 1;
-let temp = 2;
-
-//C 0
-let door = 1;
-let clk = 2;
+    /////////////////////////////////Buffer////////////////////////////
 
 
+    function sendData(actuCat: number, actuType: number, actuSet: number) {
+        let output = actuCat.toString() + ";" + actuType.toString() + "," + actuSet.toString();
+        serial.writeString(output);
+        basic.pause(100);
+    }
 
-  //% group="Sensore"
+
+    // #####################   SENSORS   #################################
+    //=============================================================================
+    //############################################################################
+
+    //ReceivedData:1|sequence|Humidity|Water_Level|CO2|Temp|Door| clock
+    //Indexing for readData
+
+
+
+    //A =0
+    let hum = 1;
+    let water = 2;
+
+    //B 0
+    //let co2 = 1;
+    let temp = 2;
+
+    //C 0
+    let door = 1;
+    let clk = 2;
+
+
+
+    //% group="Sensore"
     /**
     * Luftfugtighedsmåler
     */
     //% block
-  export function Luftfugtighedsmåler(): number {
-    let x = readData(hum, "a");
-  return x ;
-  }
-
-  /**
-  * Vandstandsmåler 0-100
-  */
-  //% block
-  //% group="Sensore"
-  export function Vandstandsmåler(): number {
-    let x = readData(water, "a");
-  return x;
-  }
-
-  /**
-  * CO2-Måler
-  */
-  //% block
-  //% group="Sensore"
-  export function co2Sensor(): number {
-    let coIn = serial.readString();
-    let coOut = coIn.split('|');
-    let coDisp = parseInt(coOut[1]);
-    if (coOut[0] == "b") {
-      serial.writeString(coOut[1]);
+    export function Luftfugtighedsmåler(): number {
+        let x = readData(hum, "a");
+        return x;
     }
 
-    return coDisp;
-  }
-
-  /**
- * Temperatursensor
- */
- //% block
- //% group="Sensore"
- export function tempSensor(): number {
-   let x = readData(temp, "b");
-  return x ;
-}
-
-
-  /**
- * DoorSensor
- */
- //% block
- //% group="Sensore"
-  export function doorSensor(): number {
-    let x = readData(door, "c");
-  return x;
-}
-
-  /**
-  * Klokken
-  */
-  //% block
-  //% group="Sensore"
-  export function clockSensor(): number {
-    let x = readData(clk, "c");
-  return x;
-}
-
-// ########################  Actuators  ######################################
-//=============================================================================
-//############################################################################
-
-//Indexing actuators
-/*A,white,
-b,uv,
-c,ir
-d,fert
-e,water
-f,air
-g,changeAIR
-h,heater
-i,fan
-j,time
-*/
-
-  /**
-  * Mock-up Light block
-  */
-  //% blockId=mockUpLight block="%lightList, Brightness %brightness"
-  //% group="Aktuatore"
-  export function groLys(lightType: lightList, lightBrigt: number){
-    let lB = lightBrigt.toString();
-    if (lightType == 0){
-      let output = "a:" + lB + "|x";
-      serial.writeString(output);
-    } else if (lightType == 1){
-      let output = "b:" + lB + "|x";
-      serial.writeString(output);
-    } else if (lightType == 2){
-        let output = "c:" + lB + "|x";
-        serial.writeString(output);
-  }
-}
-
-
-
-
-  /**
-  * Varmeboi.
-  */
-  //% blockId=airActu block="%actuList| intensitet %randNum"
-  //% group="Aktuatore"
-  export function setActuator(actu:airList, setting: number){
-    let set = setting.toString();
-    if (actu == 0){
-      let output = "h:" + set + "|x";
-      serial.writeString(output);
+    /**
+    * Vandstandsmåler 0-100
+    */
+    //% block
+    //% group="Sensore"
+    export function Vandstandsmåler(): number {
+        let x = readData(water, "a");
+        return x;
     }
-   else if (actu == 1){
-      let output = "i:" + set + "|x";
-      serial.writeString(output);
+
+    /**
+    * CO2-Måler
+    */
+    //% block
+    //% group="Sensore"
+    export function co2Sensor(): number {
+        let coIn = serial.readString();
+        let coOut = coIn.split('|');
+        let coDisp = parseInt(coOut[1]);
+        if (coOut[0] == "b") {
+            serial.writeString(coOut[1]);
+        }
+
+        return coDisp;
     }
-  }
 
-
-
-
-
-  /**
-  *Pumper.
-  */
-  //% blockId=pumpeActu block="%actuList| intensitet %randNum"
-  //% group="Aktuatore"
-  export function setPump(actu:pumpList, setting: number){
-    let set = setting.toString();
-    if (actu == 0){
-      let output = "e:" + set + "|x";
-      serial.writeString(output);
+    /**
+   * Temperatursensor
+   */
+    //% block
+    //% group="Sensore"
+    export function tempSensor(): number {
+        let x = readData(temp, "b");
+        return x;
     }
-   else if (actu == 1){
-      let output = "f:" + set + "|x";
-      serial.writeString(output);
+
+
+    /**
+   * DoorSensor
+   */
+    //% block
+    //% group="Sensore"
+    export function doorSensor(): number {
+        let x = readData(door, "c");
+        return x;
     }
-    else if (actu == 1){
-       let output = "d:" + set + "|x";
-       serial.writeString(output);
-     }
-  }
+
+    /**
+    * Klokken
+    */
+    //% block
+    //% group="Sensore"
+    export function clockSensor(): number {
+        let x = readData(clk, "c");
+        return x;
+    }
+
+    // ########################  Actuators  ######################################
+    //=============================================================================
+    //############################################################################
+
+    //Indexing actuators
+    /*A,white,
+    b,uv,
+    c,ir
+    d,fert
+    e,water
+    f,air
+    g,changeAIR
+    h,heater
+    i,fan
+    j,time
+    */
+
+    /**
+    * Mock-up Light block
+    */
+    //% blockId=mockUpLight block="%lightList, Brightness %brightness"
+    //% group="Aktuatore"
+    export function groLys(lightType: lightList, lightBrigt: number) {
+        let lB = lightBrigt.toString();
+        if (lightType == 0) {
+            let output = "a:" + lB + "|x";
+            serial.writeString(output);
+        } else if (lightType == 1) {
+            let output = "b:" + lB + "|x";
+            serial.writeString(output);
+        } else if (lightType == 2) {
+            let output = "c:" + lB + "|x";
+            serial.writeString(output);
+        }
+    }
+
+
+
+
+    /**
+    * Varmeboi.
+    */
+    //% blockId=airActu block="%actuList| intensitet %randNum"
+    //% group="Aktuatore"
+    export function setActuator(actu: airList, setting: number) {
+        let set = setting.toString();
+        if (actu == 0) {
+            let output = "h:" + set + "|x";
+            serial.writeString(output);
+        }
+        else if (actu == 1) {
+            let output = "i:" + set + "|x";
+            serial.writeString(output);
+        }
+    }
+
+
+
+
+
+    /**
+    *Pumper.
+    */
+    //% blockId=pumpeActu block="%actuList| intensitet %randNum"
+    //% group="Aktuatore"
+    export function setPump(actu: pumpList, setting: number) {
+        let set = setting.toString();
+        if (actu == 0) {
+            let output = "e:" + set + "|x";
+            serial.writeString(output);
+        }
+        else if (actu == 1) {
+            let output = "f:" + set + "|x";
+            serial.writeString(output);
+        }
+        else if (actu == 1) {
+            let output = "d:" + set + "|x";
+            serial.writeString(output);
+        }
+    }
 
 
 
@@ -293,13 +293,13 @@ j,time
     */
     //% weight=101 blockGap=8
     //% blockId=testSerialPrint block="Test Serial print: %testString"
-    export function testSerialPrint(testString: string) : void {
-      basic.showString(testString);
-      serial.writeString(testString);
+    export function testSerialPrint(testString: string): void {
+        basic.showString(testString);
+        serial.writeString(testString);
     }
 
 
-init();
+    init();
 
 }
 let bufboi = [0, 0, 0, 0, 0, 0]; //buffboi int atm
@@ -308,33 +308,33 @@ control.inBackground(function () {
 
 
 
-  function sensData(){
-    let readIn = serial.readString();
-    let inSplit = readIn.split('|');
-    if (inSplit[0] == "a") {
-      basic.showString("A"); //Debugging ReadData with string on successful read
-      basic.pause(200);
-      bufboi[0] = parseInt(inSplit[1]);
-      bufboi[1] = parseInt(inSplit[2]);
+    function sensData() {
+        let readIn = serial.readString();
+        let inSplit = readIn.split('|');
+        if (inSplit[0] == "a") {
+            basic.showString("A"); //Debugging ReadData with string on successful read
+            basic.pause(200);
+            bufboi[0] = parseInt(inSplit[1]);
+            bufboi[1] = parseInt(inSplit[2]);
 
 
-    } else if (inSplit[0] == "b") {
-      basic.showString("B"); //Debugging ReadData with string on successful read
-      basic.pause(200);
-      bufboi[2] = parseInt(inSplit[1]);
-      bufboi[3] = parseInt(inSplit[2]);
-      
-    } else if (inSplit[0] == "c") {
-      basic.showString("C"); //Debugging ReadData with string on successful read
-      basic.pause(200);
-      bufboi[4] = parseInt(inSplit[1]);
-      bufboi[5] = parseInt(inSplit[2]);
+        } else if (inSplit[0] == "b") {
+            basic.showString("B"); //Debugging ReadData with string on successful read
+            basic.pause(200);
+            bufboi[2] = parseInt(inSplit[1]);
+            bufboi[3] = parseInt(inSplit[2]);
 
+        } else if (inSplit[0] == "c") {
+            basic.showString("C"); //Debugging ReadData with string on successful read
+            basic.pause(200);
+            bufboi[4] = parseInt(inSplit[1]);
+            bufboi[5] = parseInt(inSplit[2]);
+
+        }
     }
-  }
-   while (true) {
-  sensData();
-  basic.pause(5000);
-}
+    while (true) {
+        sensData();
+        basic.pause(5000);
+    }
 
 })
